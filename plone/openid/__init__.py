bb9a5d0c65ffe5044aadcdefec3c1c054e2a7c1e
Viktor Dick <vd@perfact.de>
2019-03-06 11:46:37 +0100

    Add plugin for OpenID connect (at the moment, the code is a simple copy of the existing openid)

--- plone/openid/__init__.py
+++ plone/openid/__init__.py
@@ -3,15 +3,21 @@ from Products.PluggableAuthService.PluggableAuthService import registerMultiPlug
 
 from plone.openid import config
 
-if not config.HAS_OPENID:
+if not config.HAS_OPENID and not config.HAS_OIDC:
     import logging
     logger=logging.getLogger("Plone")
-    logger.info("OpenID system packages not installed, OpenID support not available")
+    logger.info("Neither OpenID nor OpeinID Connect system packages installed,"
+                " OpenID support not available")
 elif not config.HAS_SSL:
     import logging
     logger=logging.getLogger("Plone")
     logger.info("Python does not have SSL support. OpenID support not available")
     config.HAS_OPENID=False
+    config.HAS_OIDC = False
+elif config.HAS_OIDC:
+    config.HAS_OPENID = False
+    from plugins import oidc
+    registerMultiPlugin(oidc.OpenIdPlugin.meta_type)
 else:
     from plugins import oid
     registerMultiPlugin(oid.OpenIdPlugin.meta_type)
@@ -19,7 +25,16 @@ else:
 
 
 def initialize(context):
-    if config.HAS_OPENID:
+    if config.HAS_OPENIDC:
+        context.registerClass(oidc.OpenIdPlugin,
+                                permission=ManageUsers,
+                                constructors=
+                                        (oidc.manage_addOpenIdPlugin,
+                                        oidc.addOpenIdPlugin),
+                                visibility=None,
+                                icon="www/openid.png")
+
+    elif config.HAS_OPENID:
         context.registerClass(oid.OpenIdPlugin,
                                 permission=ManageUsers,
                                 constructors=
--- plone/openid/config.py
+++ plone/openid/config.py
@@ -7,6 +7,13 @@ except pkg_resources.DistributionNotFound:
 else:
     HAS_OPENID = True
 
+try:
+    pkg_resources.get_distribution('oic')
+except pkg_resources.DistributionNotFound:
+    HAS_OIDC = False
+else:
+    HAS_OIDC = True
+
 import socket
 HAS_SSL = hasattr(socket, "ssl")
 del socket
